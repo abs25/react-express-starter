6dde6aa92e4a1c93813572e59b89b56e67004f13
Austin Snoeyink <nxtexracer@gmail.com>
2020-05-08 20:50:14 -0400

    webpack generating bundle and html file to dist

--- modules/client/index.tsx
+++ modules/client/index.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import * as ReactDOM from "react-dom";
 
 export const App: React.FC<{}> = (props) => {
   return <>Hello World!</>;
 };
+
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack/client.config.js
+++ webpack/client.config.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: "./modules/client/index.tsx",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "../dist"),
   },
   module: {
     rules: [
